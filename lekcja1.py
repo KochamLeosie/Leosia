180dac16cb490e26c3c469f8ad98a23f26e23987
KochamLeosie <98099418+KochamLeosie@users.noreply.github.com>
2022-01-25 11:19:15 +0100

    Add files via upload

--- lekcja1.py
+++ lekcja1.py
@@ -1,12 +1,16 @@
-from pip import main
 import pygame
+
 def main():
-    pygame.init()
-    OknoGry=pygame.display.set_mode((400,400),0,32)
-    run=True
-    while(run):
-            pygame.time.delay(200)
-            for event in pygame.event.get():
-                    if event.type == pygame.QUIT:
-                        run=False
-main ()
+   pygame.init()
+   OknoGry=pygame.display.set_mode((400,400),0,32)
+   run=True
+   while(run):
+       pygame.time.delay(200)
+       wazShape=pygame.Rect((140,140),(47,47))
+       pygame.draw.rect(OknoGry,(255,192,203),wazShape)
+       for event in pygame.event.get():
+           if event.type == pygame.QUIT:
+               run=False
+       pygame.display.update()
+
+main() 
